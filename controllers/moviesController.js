683859129639d600e3e8c1a7c07c55dca41f0852
mannuccimattia <mannuccimattia@gmail.com>
2025-05-29 16:11:39 +0200

    [FEAT] Exported query error message to function queryFailed

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,14 +1,15 @@
 // import db
 const connection = require("../data/db_copia");
 
+// import function queryFailed
+const queryFailed = require("../functions/queryFailed");
+
 // index
 const index = (req, res) => {
   const sql = "SELECT * FROM movies";
 
   connection.query(sql, (err, moviesResult) => {
-    if (err) return res.status(500).json({
-      error: "DB query failed: " + err
-    })
+    if (err) return queryFailed(err, res);
     const movies = moviesResult.map(movie => {
       const mov = { ...movie, image: req.imagePath + movie.image };
 
@@ -36,21 +37,15 @@ const show = (req, res) => {
   `
 
   connection.query(movieSql, [id], (err, movieResult) => {
-    if (err)
-      return res.status(500).json({
-        error: "DB query failed: " + err
-      })
+    if (err) return queryFailed(err, res);
     if (movieResult.length === 0 || movieResult[0].id === null)
       return res.status(404).json({
         error: "Movie not Found"
-      })
+      });
     const movie = movieResult[0];
 
     connection.query(reviewsSql, [id], (err, reviewsResult) => {
-      if (err)
-        return res.status(500).json({
-          error: "DB query failed: " + err
-        })
+      if (err) return queryFailed(err, res);
       movie.reviews = reviewsResult;
 
       res.json({ ...movie, image: req.imagePath + movie.image });
