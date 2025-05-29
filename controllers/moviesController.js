b50f8cfcd5f4935d2339ca2d2552732d0b9597d9
mannuccimattia <mannuccimattia@gmail.com>
2025-05-29 15:12:04 +0200

    [UPD] Done index route

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,7 +1,15 @@
 const connection = require("../data/db_copia");
 
 const index = (req, res) => {
-  res.send("Elenco film");
+  const sql = "SELECT * FROM movies";
+
+  connection.query(sql, (err, moviesResult) => {
+    if (err) return res.status(500).json({
+      error: "DB query failed: " + err
+    })
+
+    res.json(moviesResult);
+  })
 }
 
 const show = (req, res) => {
